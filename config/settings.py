72eca83d1e95ceefe0e111679a7443becf948fe8
kholmumin <xolmomin@gmail.com>
2020-04-02 23:59:36 +0500

    make it 7

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/', 'live-static', 'static-root')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/', 'live-static', 'static-root')
